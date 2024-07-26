45df42f6c614a08e5f0d4cd63aef34c246e7d54d
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2024-07-26 15:19:07 -0300

    feat: add variable enviromment

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
@@ -5,7 +8,12 @@ plugins {
     //plugin para serilazation
     kotlin("plugin.serialization") version("1.9.20")
 
+    //plugin para gerar build config
+    id("com.github.gmazzo.buildconfig") version "5.4.0"
+
+
 }
+
 
 kotlin {
     androidTarget {
@@ -15,6 +23,19 @@ kotlin {
             }
         }
     }
+
+    //https://stackoverflow.com/questions/74743976/kotlin-multiplatform-accessing-build-variables-in-code
+    //kotlin multiplataform precisa de plugin
+
+    //local onde esta o arquiivo local.properties
+    //https://stackoverflow.com/questions/20673378/where-does-local-properties-go-for-android-project
+    val properties = Properties()
+    properties.load(project.rootProject.file("local.properties").inputStream())
+    val apiKey = properties.getProperty("API_KEY")
+    buildConfig {
+        buildConfigField("String", "API_KEY", "\"$apiKey\"")
+    }
+
 
     listOf(
         iosX64(),
--- shared/src/commonMain/kotlin/com/example/articleskmp/client/ArticlesClient.kt
+++ shared/src/commonMain/kotlin/com/example/articleskmp/client/ArticlesClient.kt
@@ -1,14 +1,17 @@
 package com.example.articleskmp.client
 
+import Articles_KMP.shared.BuildConfig
 import com.example.articleskmp.model.ArticleEntity
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
 class ArticlesClient(private val ktorApi: KtorApi) : KtorApi by ktorApi {
+   private  val apikey = BuildConfig.API_KEY
+
 
     suspend fun fetchArticles(): ArticleEntity {
         return client.get {
-            apiUrl("/v2/top-headlines?country=us&category=business&apiKey=e03da12b408445449464ceb16db4963a")
+            apiUrl("/v2/top-headlines?country=us&category=business&apiKey=$apikey")
         }.body()
     }
 
